45b4b27349d0ba292ef9cb1d82a3302ac3672e0e
AREE93X <angel.enriquez.esp@outlook.com>
2025-07-15 12:47:30 -0600

    version 2

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
-dataraw = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Venta de vehículos')
-histo_button = st.button('Generar histográma')
+histo_check = st.checkbox('Generar histográma')
+disper_check = st.checkbox('Generar gráfico de dispersión')
 
-if histo_button:
+if histo_check:
     st.write('Creación de un histográma para el conjunto de datos de anuncios de venta de coches')
-    fig = go.Figure(data=[go.Histogram(x=dataraw['odometer'])])
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+    fig.update_layout(title_text='Distribución del Odómetro')
+    st.plotly_chart(fig, use_container_width=True)
+if disper_check:
+    st.write('Creación de diagráma de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'])])
     fig.update_layout(title_text='Distribución del Odómetro')
     st.plotly_chart(fig, use_container_width=True)
 
